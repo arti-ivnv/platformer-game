e50bcb17baf804e88956da7d800a5cabec58e8f8
Artem Ivanov <arti.ivnv@gmail.com>
2023-10-18 18:31:21 +0300

    replay button fixed

--- HelloPlatformer/src/ui/PauseOverlay.java
+++ HelloPlatformer/src/ui/PauseOverlay.java
@@ -144,7 +144,8 @@ public class PauseOverlay {
             }
         } else if (isIn(e, replayB)){
             if(replayB.isMousePressed()){
-               System.out.println("replay level!");
+               playing.resetAll();
+               playing.unpauseGame();
             }
         } else if (isIn(e, unpauseB)){
             if(unpauseB.isMousePressed()){
